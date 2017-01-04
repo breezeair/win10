b698bd0ec1b88b93c371cc3db24db39d0519c3bc
Kevin Ye (Shang Hai Wei Chuang Ruan Jian) <v-kuye@microsoft.com>
2017-01-04 18:15:16 +0800

    modified in office 3.

--- App6AboutUI/App6AboutUI/View/Page1.xaml.cs
+++ App6AboutUI/App6AboutUI/View/Page1.xaml.cs
@@ -25,12 +25,24 @@ namespace App6AboutUI.View
     /// </summary>
     public sealed partial class Page1 : Page
     {
+        //set up in configuration cs.
+        public const string FEATURE_NAME = "SystemBack";
+
+        //List<Scenario> scenarios = new List<Scenario>
+        //{
+        //    new Scenario() { Title="Subscribe to the BackRequested event", ClassType=typeof(Scenario1)},
+        //};
+        //set up in configuration cs.
+
         public Page1()
         {
+
             this.InitializeComponent();
+
+
         }
 
-        private async void Button_Click(object sender, RoutedEventArgs e)
+        /*private async void Button_Click(object sender, RoutedEventArgs e)
         {
             //Create a new view
             CoreApplicationView newView = CoreApplication.CreateNewView();
@@ -48,7 +60,6 @@ namespace App6AboutUI.View
             bool viewShown = await ApplicationViewSwitcher.TryShowAsStandaloneAsync(newViewId);
 
         }
-
         private async void Button_Click_1(object sender, RoutedEventArgs e)
         {
             ContentDialog noWifiDialog = new ContentDialog()
@@ -59,6 +70,46 @@ namespace App6AboutUI.View
             };
 
             ContentDialogResult result = await noWifiDialog.ShowAsync();
+        }*/
+
+        private void ToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            Splitter.IsPaneOpen = !Splitter.IsPaneOpen;
+        }
+
+        async void Footer_Click(object sender, RoutedEventArgs e)
+        {
+            await Windows.System.Launcher.LaunchUriAsync(new Uri(((HyperlinkButton)sender).Tag.ToString()));
+        }
+
+        private void ScenarioControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Clear the status block when navigating scenarios.
+           /* NotifyUser(String.Empty, NotifyType.StatusMessage);
+
+            ListBox scenarioListBox = sender as ListBox;
+            Scenario s = scenarioListBox.SelectedItem as Scenario;
+            if (s != null)
+            {
+                ScenarioFrame.Navigate(s.ClassType);
+                if (Window.Current.Bounds.Width < 640)
+                {
+                    Splitter.IsPaneOpen = false;
+                }
+            }*/
+        }
+
+        public List<Scenario> Scenarios
+        {
+            get { return this.scenarios; }
         }
     }
+
+    public class Scenario
+    {
+        public string Title { get; set; }
+        public Type ClassType { get; set; }
+    }
+
+
 }
