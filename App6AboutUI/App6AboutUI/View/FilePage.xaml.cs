a9504bd34c005c30e3f9a4a606e48d6a4e877d0b
Kevin Ye (Shang Hai Wei Chuang Ruan Jian) <v-kuye@microsoft.com>
2017-01-06 17:27:56 +0800

    modified in office 6.

--- App6AboutUI/App6AboutUI/View/FilePage.xaml.cs
+++ App6AboutUI/App6AboutUI/View/FilePage.xaml.cs
@@ -27,12 +27,33 @@ namespace App6AboutUI.View
         public FilePage()
         {
             this.InitializeComponent();
+            // Data source.
+            List<String> itemsList = new List<string>();
+            itemsList.Add("Item 1");
+            itemsList.Add("Item 2");
+
+            // Create a new flip view, add content, 
+            // and add a SelectionChanged event handler.
+            FlipView flipView1 = new FlipView();
+            flipView1.ItemsSource = itemsList;
+            flipView1.SelectionChanged += FlipView_SelectionChanged;
+
+            // Add the flip view to a parent container in the visual tree.
+            stackPanel1.Children.Add(flipView1);
+        }
+
+        private void FlipView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         private async void button_Click(object sender, RoutedEventArgs e)
         {
             StorageFolder picturesFolder = KnownFolders.PicturesLibrary;
+            //StorageFolder picturesFolder = ApplicationData.Current.LocalFolder;
+            //StorageFolder picturesFolder = await StorageFolder.GetFolderFromPathAsync(@"D:\");
             StringBuilder outputText = new StringBuilder();
+            
 
             IReadOnlyList<StorageFile> fileList =
                 await picturesFolder.GetFilesAsync();
@@ -53,6 +74,32 @@ namespace App6AboutUI.View
             }
             string ss = outputText.ToString();
             textBlock1.Text = ss;
+
+            myStoryboard.Begin();
+        }
+
+        private async void button2_Click(object sender, RoutedEventArgs e)
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
+            picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+            picker.SuggestedStartLocation =
+            Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
+
+            //picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.Desktop;
+        picker.FileTypeFilter.Add(".jpg");
+            picker.FileTypeFilter.Add(".jpeg");
+            picker.FileTypeFilter.Add(".png");
+
+            Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
+            if (file != null)
+            {
+                // Application now has read/write access to the picked file
+                textBlock2.Text = "Picked photo: " + file.Name;
+            }
+            else
+            {
+                textBlock2.Text = "Operation cancelled.";
+            }
         }
     }
 }
